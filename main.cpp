8dd1ab45f3574b8c9e85da34bb5f45fe8b831075
HonfiBotond <hobot2002@gmail.com>
2023-09-24 21:50:51 +0200

    Std_out

--- main.cpp
+++ main.cpp
@@ -22,5 +22,8 @@ int main()
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
+    
+    std::cout << "Ezt a kódot ide én írtam :P" << std::endl;
+
     return 0;
 }
